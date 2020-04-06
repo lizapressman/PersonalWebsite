b129db7974992a16c2734d259e97ea7c01eb6178
lizapressman <lizarogot@gmail.com>
2020-04-06 02:24:50 -0700

    portfolio overlay and contact icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,8 +14,20 @@ export default class Portfolio extends Component {
         <div id="contact" style={{"background-image": "url(./images/fall.jpeg)"}}>
           <div class = "title" >
               <h1>Contact Me</h1>
+              {/* </div> */}
               {/* <p style={{"color": "black"}}>Phone: (510) 410-0565<br></br>Email: lpressma@u.rochester.edu</p> */}
-              <p><span style={{"background-color": "white", "padding":"2vh","color": "black"}}>Phone: (510) 410-0565</span><br></br><br></br><span style={{"background-color": "white", "padding":"2vh", "color": "black"}}>Email: lpressma@u.rochester.edu</span></p>
+
+              <p style={{"background-color": "white", "padding":"2vh","margin-left":"40vw","margin-right":"40vw","color": "black"}}>
+                <a href="mailto:lpressma@u.rochester.edu"><img src="https://img.icons8.com/nolan/64/email.png"/></a>
+                {/* <a href="mailto:lpressma@u.rochester.edu"><img src="https://img.icons8.com/nolan/64/filled-sent.png"/></a> */}
+                <a href="https://github.com/lizapressman"><img src="https://img.icons8.com/nolan/64/github.png"/></a>
+                <a href="https://www.linkedin.com/in/lizapressman/"><img src="https://img.icons8.com/nolan/64/linkedin.png"/></a> 
+              </p> 
+              {/* <p style={{"background-color": "white", "padding":"2vh","margin-left":"35vw","margin-right":"35vw","color": "black"}}>
+                <a href="mailto:lpressma@u.rochester.edu"><img src="https://img.icons8.com/carbon-copy/100/000000/email.png"/></a>
+                <a href="https://github.com/lizapressman"><img src="https://img.icons8.com/wired/64/000000/github.png"/></a>
+                <a href="https://www.linkedin.com/in/lizapressman/"><img src="https://img.icons8.com/wired/64/000000/linkedin.png"/></a> 
+              </p>                       */}
           </div>
         </div>
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,35 +10,131 @@ export default class Portfolio extends Component {
 
 		<div class="row">
 			<div class="column">
-				{/* <div class="imgcontainer">
-					<img src="art/1.jpg" style="width:100%" class="image">
+				<div class="imgcontainer">
+					<img src="art/1.jpg" class="image"/>
 						<div class="overlay">
 							<div class="text">Graphite and colored pencil shavings.</div>
 						</div>
-				</div> */}
-				<img src="art/1.jpg"></img>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/3.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Graphite hand study.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/4.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Watered down acrylic paint on synthetic paper.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/17.png" class="image"/>
+						<div class="overlay">
+							<div class="text">Anime created with Procreate on an ipad pro.</div>
+						</div>
+				</div>
+				{/* <img src="art/1.jpg"></img>
 			  	<img src="art/3.jpg"></img>
 			  	<img src="art/4.jpg"></img>
-			  	<img src="art/17.png"></img>
+			  	<img src="art/17.png"></img> */}
 			</div>
 			<div class="column">
-			  	<img src="art/5.jpg"></img>
+				<div class="imgcontainer">
+					<img src="art/5.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Acrylic and pen on canvas.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/6.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Graphite sketch.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/2.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Acrylic on canvas.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/8.png" class="image"/>
+						<div class="overlay">
+							<div class="text">Stickers created with Procreate on an ipad pro.</div>
+						</div>
+				</div>
+			  	{/* <img src="art/5.jpg"></img>
 			  	<img src="art/6.jpg"></img>
 			  	<img src="art/2.jpg"></img>
-			  	<img src="art/8.png"></img>
+			  	<img src="art/8.png"></img> */}
 			</div>
 			<div class="column">
-			  	<img src="art/9.jpg"></img>
+			<div class="imgcontainer">
+					<img src="art/9.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">My aunt's dog, Summit, created with Procreate on an ipad pro.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/10.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Paper and pen on a black pedastal.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/11.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Marker anime.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/12.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Graphite sketch.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/16.png" class="image"/>
+						<div class="overlay">
+							<div class="text">Sketch created with Procreate on an ipad pro.</div>
+						</div>
+				</div>
+			  	{/* <img src="art/9.jpg"></img>
 			  	<img src="art/10.jpg"></img>
 			  	<img src="art/11.jpg"></img>
 			  	<img src="art/12.jpg"></img>
-			  	<img src="art/16.png"></img>
+			  	<img src="art/16.png"></img> */}
 			</div>
 			<div class="column">
-			  	<img src="art/13.png"></img>
+			<div class="imgcontainer">
+					<img src="art/13.png" class="image"/>
+						<div class="overlay">
+							<div class="text">Sketch created with Procreate on an ipad pro.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/14.jpg" class="image"/>
+						<div class="overlay">
+							<div class="text">Pen zentangle.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/15.png" class="image"/>
+						<div class="overlay">
+							<div class="text">My dog, Coconut, created with Procreate on an ipad pro.</div>
+						</div>
+				</div>
+				<div class="imgcontainer">
+					<img src="art/7.JPG" class="image"/>
+						<div class="overlay">
+							<div class="text">Box sculpture made from found objects.</div>
+						</div>
+				</div>
+			  	{/* <img src="art/13.png"></img>
 			  	<img src="art/14.jpg"></img>
 			  	<img src="art/15.png"></img>
-			  	<img src="art/7.JPG"></img>
+			  	<img src="art/7.JPG"></img> */}
 			</div>
 		  </div>
         </div>
