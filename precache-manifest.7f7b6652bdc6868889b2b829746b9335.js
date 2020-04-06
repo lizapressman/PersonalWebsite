8686e0788ab4eec304974bee64937b56ae82c314
lizapressman <lizarogot@gmail.com>
2020-04-05 22:48:37 -0700

    Updates

---
+++ precache-manifest.7f7b6652bdc6868889b2b829746b9335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb776751666010d309d66fe6c3a1711",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "cc005e469230bd442326",
+    "url": "/reactwebsite/static/css/main.33fc1e01.chunk.css"
+  },
+  {
+    "revision": "48df6cbbf7874f14ed70",
+    "url": "/reactwebsite/static/js/2.7f792868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwebsite/static/js/2.7f792868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc005e469230bd442326",
+    "url": "/reactwebsite/static/js/main.f78c4909.chunk.js"
+  },
+  {
+    "revision": "6779b68379bfe8a18190",
+    "url": "/reactwebsite/static/js/runtime-main.a66ae2a9.js"
+  }
+]);
