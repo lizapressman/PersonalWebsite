de27a725a4a077eeea952711f7352b357794d1bc
LizaPressman <LizaPressman@target.com>
2020-06-30 19:15:15 -0400

    Updates

--- precache-manifest.c9ca78983fff7de1fb4012cc03b4c5b2.js
+++ precache-manifest.c9ca78983fff7de1fb4012cc03b4c5b2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4d0667266f0af69239c1369ebac485af",
+    "revision": "9509a6fa736e3373adc2009be8c373e9",
     "url": "/reactwebsite/index.html"
   },
   {
-    "revision": "02c4e1017c2b9b4196fa",
+    "revision": "e7fdfbe2d532280170f8",
     "url": "/reactwebsite/static/css/main.57ac39b6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactwebsite/static/js/2.b2d9b503.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "02c4e1017c2b9b4196fa",
-    "url": "/reactwebsite/static/js/main.2644acd6.chunk.js"
+    "revision": "e7fdfbe2d532280170f8",
+    "url": "/reactwebsite/static/js/main.bfdf2591.chunk.js"
   },
   {
     "revision": "21d76a68f94d2ebbc15f",
