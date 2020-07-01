849603f4d3c1ac671af9ae3348aa4c50a989a048
LizaPressman <LizaPressman@target.com>
2020-07-01 11:24:13 -0400

    rand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ export const Header = () => {
   return (
 
     <div id="header" style={{ "backgroundImage": "url(./images/salt.jpeg)" }}>
+      {/* // <div id="header"> */}
       <div className="title">
         <h3>Hey,</h3>
         <h1>I'M LIZA!</h1>
