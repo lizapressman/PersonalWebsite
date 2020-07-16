596836757fbab103f1fa9041839396472cd14960
Liza Pressman <lizapressman@Lizas-MacBook-Pro.local>
2020-07-16 00:53:20 -0400

    experience

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,24 +3,9 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-
-    // <div id="contact">
-    //     <h1>Contact Me</h1>
-    //     {/* <hr></hr> */}
-    //     <p>Phone: (510) 410-0565 <br></br> Email: lpressma@u.rochester.edu</p>
-    // </div>
-
     <div id="contact" style={{ "backgroundImage": "url(./images/fall.jpeg)" }}>
       <div className="title" >
-        <h1>Contact Me</h1>
-        {/* </div> */}
-        {/* <p style={{"color": "black"}}>Phone: (510) 410-0565<br></br>Email: lpressma@u.rochester.edu</p> */}
-
-        {/* <p style={{ "backgroundColor": "white", "padding": "2vh", "marginLeft": "40vw", "marginRight": "40vw", "color": "black" }}>
-          <a href="mailto:lpressma@u.rochester.edu"><img src="https://img.icons8.com/nolan/64/email.png" /></a>
-          <a href="https://github.com/lizapressman"><img src="https://img.icons8.com/nolan/64/github.png" /></a>
-          <a href="https://www.linkedin.com/in/lizapressman/"><img src="https://img.icons8.com/nolan/64/linkedin.png" /></a>
-        </p> */}
+        <h1>Contact Me</h1>=
         <p>
           <a href="https://github.com/lizapressman"><img src="https://img.icons8.com/wired/64/000000/github.png" /></a>
           <a href="mailto:lpressma@u.rochester.edu"><img src="https://img.icons8.com/carbon-copy/100/000000/email.png" /></a>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,57 +7,59 @@ export const Experience = () => {
 
 			<h1>Experience</h1>
 			<hr></hr>
-
-
-			{/* <h2><b>Internship</b></h2>
-			<p>Last summer I had a two month software engineering internship with <i>Doctorbox Health</i> in Sausalito, CA.
-					Working with a small startup was a great experience. The fast paced, 'quick to change' environment was brand new to me and I learned so much from it.
-					This was also a great glimpse into the technology meets healthcare world that I was (and still am) really interesting in diving into. </p>
-			<p>Since the work I was doing was so fast paced I got the opportunity to work on many different projects throughout the summer.
-						<ul>
-					<li>My primary responsibility was to work on what they called the "facesheet."
-					The facesheet is a single pdf document that takes a bunch of patient recorded data as input and summarizes it in a meaningful, easy to read way for the surgeons' use.
-					The majority of my job here was to
-					1. figure out how optimize the visual display (what to graph, what to list, what  to combine with what, etc.) and
-							2. use java to actually display it (read in the numbers and output something pretty).</li>
-					<li>My next task was to convert the patient recorded data to natural language. This I completed using javascript.
-						Before starting this task the CTO taught me a bunch of stuff with AWS which was really interesting since I had never used any of those services prior to my internship. </li>
-					<li>Finally, the last week of my internship I had pretty much completed everything they wanted but in such a fast paced environment, there is always stuff to be done so the CTO let me pick what I wanted to learn.
-					The company was in the process of creating a web app throughout my whole internship and the questionnaire page had not been completed yet so I got to develop a bit of that page in my final week.
-						This was my first time using Vue.js and it was really neat to get experience with something totally new!</li>
-				</ul>
-			</p> */}
 			<div className="experienceBox">
 				<h2><b>Target</b></h2>
 				<h3>Software Engineering Intern | June 2020 - PRESENT</h3>
-				{/* <p>Did oh so many a tutorial.</p> */}
+				<p>Working on the Purchase Orders Team developing with Java, Springboot, jOOQ, and React
+					<ul>
+						<li>Writing React components for internal Admin UI</li>
+						<li>Writing PostgreSQL queries within Java Springboot backend and exposing them on rest endpoints</li>
+						<li>Writing Java Springboot functional and unit tests</li>
+					</ul>
+				</p>
 			</div>
 
 			<div className="experienceBox">
 				<h2><b>DoctorPlan</b></h2>
 				<h3>Software Engineering Intern | June 2019 - August 2019</h3>
-				<p>Last summer I had a two month software engineering internship with <i>Doctorbox Health</i> in Sausalito, CA.
-					Working with a small startup was a great experience. The fast paced, 'quick to change' environment was brand new to me and I learned so much from it.
-					This was also a great glimpse into the technology meets healthcare world that I was (and still am) really interesting in diving into. </p>
-				<p>Since the work I was doing was so fast paced I got the opportunity to work on many different projects throughout the summer.
+				<p>Worked primarily on doctor facing tools
 					<ul>
-						<li>My primary responsibility was to work on what they called the "facesheet."
-						The facesheet is a single pdf document that takes a bunch of patient recorded data as input and summarizes it in a meaningful, easy to read way for the surgeons' use.
-						The majority of my job here was to
-						1. figure out how optimize the visual display (what to graph, what to list, what  to combine with what, etc.) and
-								2. use java to actually display it (read in the numbers and output something pretty).</li>
-						<li>My next task was to convert the patient recorded data to natural language. This I completed using javascript.
-							Before starting this task the CTO taught me a bunch of stuff with AWS which was really interesting since I had never used any of those services prior to my internship. </li>
-						<li>Finally, the last week of my internship I had pretty much completed everything they wanted but in such a fast paced environment, there is always stuff to be done so the CTO let me pick what I wanted to learn.
-						The company was in the process of creating a web app throughout my whole internship and the questionnaire page had not been completed yet so I got to develop a bit of that page in my final week.
-							This was my first time using Vue.js and it was really neat to get experience with something totally new!</li>
+						<li>Used Java to gather and summarize patient data and optimize visual display for surgeons’ use</li>
+						<li>Used Javascript to convert data to natural language</li>
+						<li>Used Vue.js to develop a questionnaire web application</li>
 					</ul>
 				</p>
 			</div>
 
-
+			<h1>Projects</h1>
+			<hr></hr>
 			<div className="experienceBox">
-				<h2><b>Courses</b></h2>
+				<h2><b>Personal Website | React</b></h2>
+				<a href="https://github.com/lizapressman/PersonalWebsite">github.com/lizapressman/PersonalWebsite</a>
+				<p>Personal portfolio site written in React using Typescript and custom CSS</p>
+			</div>
+			<div className="experienceBox">
+				<h2><b>ResumeToWebsite | React</b></h2>
+				<a href="https://github.com/lizapressman/ResumeToWebsite">github.com/lizapressman/ResumeToWebsite</a>
+				<p>Resume Parser that produces a resulting html portfolio site based on the resume</p>
+			</div>
+			<div className="experienceBox">
+				<h2><b>Therapy Journaling Application | React</b></h2>
+				<a href="https://github.com/Mayshinlyan/TherapyJournalling">github.com/Mayshinlyan/TherapyJournalling</a>
+				<p>Application using MERN stack for personalized journaling with automated feedback based on mood &amp; text analysis</p>
+			</div>
+			<div className="experienceBox">
+				<h2><b>Dijkstra’s Algorithm | Java</b></h2>
+				<p>Implemented a program using java graphics to find the shortest path between any two points in New York State</p>
+			</div>
+			<div className="experienceBox">
+				<h2><b>Connect Four AI | Java</b></h2>
+				<p>Implemented a minimax algorithm along with a heuristic function and alpha-beta pruning (with a fixed depth cutoff)</p>
+			</div>
+
+			<h1>Courses</h1>
+			<hr></hr>
+			<div className="experienceBox">
 				<div className="container">
 					<div>
 						<h2>Computer Science</h2>
@@ -74,7 +76,7 @@ export const Experience = () => {
 						<h2>Mathematics</h2>
 						<ul>
 							<li>Discrete Mathematics</li>
-							<li>Calculus (differential, integral, multidimentional)</li>
+							<li>Calculus (differential, integral, multi)</li>
 							<li>Linear Algebra</li>
 							<li>Introduction to Cryptography</li>
 							<li>Introduction to Topology</li>
@@ -95,18 +97,6 @@ export const Experience = () => {
 						</ul>
 					</div>
 				</div>
-			</div>
-
-			<div className="experienceBox">
-				<h2><b>Projects</b></h2>
-				<ul>
-					<li>In <i>Human Computer Interaction</i>, I had a group project spanning almost the length of the class in which we created a web application using MERN stack for user’s personalized journaling with automated feedback on user’s private journals, along with mood and text analysis. You can check out the working prototype here: <a href='https://journaly-csc.herokuapp.com' />Journaly.</li>
-					<li>In <i>Artificial Intelligence</i>, I implemented a connect four AI that beats me everytime (which is totally awesome)! I struggled through creating an AI for connect four in highschool and my final product was really bad since I didn't use any proper algorithms. But, it was incredible to see such a transformation between my two AIs. I also implemented a program that can solve any constraint satisfaction problem (CSP). Finally for that course, I coded a decision tree learning program as well as a Linear and a Perceptron classifier.</li>
-					<li>In <i>Computation and Formal Models</i> I created projects which involved creating DFAs, NFAs, Recursive and table driven parsers for grammars, functional programming, and implementing my own relational data model. This was definitely my favorite course. All coding had to be done in C which we had to teach ourselves so it was very challenging. It was also my first CS theory course so it was quite an adjustment. However, it was the most rewarding course i've taken thus far.</li>
-					<li>In <i>Data Structures and Algorithms</i>, I worked on some very cool projects. I created a version of the popular game 2048. This was my first time working with graphics in java and I was pretty proud of my final product. In this class, I also worked on a huffman encoding project which was one of my favorites because the actual application was so evident. It was also really exciting to see everything come together and work as it should. My last project for this course involved using Dijkstras algorithm to find the shortest path between any two points in New York state. Visually, this was the most exciting project.</li>
-					<li>In <i>Web design</i>, I created a wide array of websites. I used what I learned in this course to create my first versions of my personal website with just basic html, css, and javascript.</li>
-					<li>Finally, I learned React to create this website that you're looking at now!</li>
-				</ul>
 			</div>
 		</div>
 
